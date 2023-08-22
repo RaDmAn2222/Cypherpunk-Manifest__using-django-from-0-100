c18aa4bb7bea47e313c274532b2d84c12ff86dbe
Radman2222 <radmanhabibi2222@gmail.com>
2023-08-22 11:58:09 +0330

    Upload django project on Github

---
+++ radman2222_project/website/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+
+def home(request):
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render())
+
+def about(request):
+    template = loader.get_template('about.html')
+    return HttpResponse(template.render())
+
+def info(request):
+    template = loader.get_template('info.html')
+    return HttpResponse(template.render())
